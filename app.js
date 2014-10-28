a968fbe8339db7486294de04d89fd00820260c2d
Alvaro Belmonte <abelmonte4eso2@gmail.com>
2014-10-28 17:01:40 +0100

    Added p when button is pressed

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var main = function  () {
 	"use strict";
 
 	$(".comment-input button").on("click", function (event){
-		console.log("Hello world!");
+		//console.log("Hello world!");
+		$(".comments").append("<p>This is a new comment</p>");
 	});
 };
 
