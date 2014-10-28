10d92e1b918f379a38efe4c801b9a2f9d5460336
Alvaro Belmonte <abelmonte4eso2@gmail.com>
2014-10-28 17:10:13 +0100

    Added comment when button is pressed

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ var main = function  () {
 
 	$(".comment-input button").on("click", function (event){
 		//console.log("Hello world!");
-		$(".comments").append("<p>This is a new comment</p>");
+		//$(".comments").append("<p>This is a new comment</p>");
+		var $new_comment = $("<p>"),
+			comment_text = $(".comment-input input").val();
+
+
+		$new_comment.text(comment_text);
+		$(".comments").append($new_comment);
 	});
 };
 
